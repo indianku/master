7e3b329240f1756f784029e713964a7d73710cdc
indianku <106584664+indianku@users.noreply.github.com>
2022-08-24 14:59:31 +0530

    Update HeaderClass.java

--- AutomationPractice/src/main/java/pomclasses/HeaderClass.java
+++ AutomationPractice/src/main/java/pomclasses/HeaderClass.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+chetan
 public class HeaderClass {
 	private WebDriver driver;
 	private WebDriverWait wait;
